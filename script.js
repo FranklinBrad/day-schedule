90de9124ee71ada44ed73622829cb1ffcf4b3794
FranklinBrad <franklin1jbradley@gmail.com>
2023-10-12 00:19:42 -0500

    finished

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var hours = ["9AM","10AM","11AM","12PM",
-"1PM","2PM","3PM","4PM","5PM"]
-var saveBtn = document.querySelector(".btn saveBtn col-2 col-md-1")
 currentTimer = setInterval(function () {
-  var currDay = dayjs().format('MMM D, YYYY, hh:mm:ss a')
-  $('#this-day').text(currDay);
-  }, 1000);
+  var currDay = dayjs().format("MMM D, YYYY, hh:mm:ss a");
+  $("#this-day").text(currDay);
+}, 1000);
 
 $(function () {
   $(".saveBtn").on("click", function (event) {
     console.log("click");
+    var time = $(this).parent().attr("id");
+    var value = $(this).siblings(".description").val();
+    console.log(time, value);
+    localStorage.setItem(time, value);
   });
+  for (var i = 9; i < 18; i++) {
+    $("#hour-" + i + " .description").val(localStorage.getItem("hour-" + i));
+  }
+
+  function hourUpdater() {
+    var currHour = dayjs().hour();
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
+      if (blockHour < currHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
+  }
+hourUpdater()
+setInterval(hourUpdater, 15000)
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -31,25 +51,15 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-  
 });
 
-function makeTimeBlocks(){
+function makeTimeBlocks() {
   var boxes = $(".container");
- 
-
-
-
-
-
-
-
 }
 
-
 //add more time blocks
-  
+
 //figure out how to change colors for different times
-  // if statments and time arr
-  
+// if statments and time arr
+
 //save tasks in local storage
